385eb26f41eb319e0e3045b166c07e8b58fbb81d
sanchezcas <sanchezcas@esatvalencia.lan>
2026-01-08 20:20:53 +0100

    save code

--- Exercise_2.cc
+++ Exercise_2.cc
@@ -8,6 +8,14 @@ void DeleteSpecialCharacters(char Cadena[80]) {
     Cadena[strlen(Cadena) - 1] = '\0';
 }
 
+void ReverseWord(char Cadena[80]) {
+    for (int i = 0; i < strlen(Cadena); i++) {
+        Cadena[i] = Cadena[strlen(Cadena) - i - 1];
+    }
+
+    fputs(Cadena, stdout);
+}
+
 int main() {
 
     int Tries = 0;
@@ -18,7 +26,8 @@ int main() {
         fgets(Answer, 80, stdin);
         DeleteSpecialCharacters(Answer);
 
-        
+        ReverseWord(Answer);
+
         if (strcmp(CorrectAnswer, strlwr(Answer)) != 0) {
             strcat(TotalWords, Answer);
             strcat(TotalWords, NextLine);

89acfaaabb37bbb83525ebbc2db735905940d334
sanchezcas <sanchezcas@esatvalencia.lan>
2026-01-13 20:15:04 +0100

    new exercises

---
+++ Exercise_6.cc
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+char Cadena[255] = {'\0'}, FirstWord[255] = {'\0'}, SecondWord[255] = {'\0'};
+
+int strlen(char Cadena[]) {
+    int contador = 0;
+
+    while (Cadena[contador] != '\0') {
+        contador++;
+    }
+
+    return contador;
+}
+
+void DeleteSpecialCharacters(char Cadena[]) {
+
+    Cadena[strlen(Cadena) - 1] = '\0';
+}
+
+void ReverseWord(char Cadena[]) {
+    
+    int Length = strlen(Cadena);
+    char Aux;
+
+    for (int i = 0; i < Length / 2; i++) {
+        // Necesito una auxiliar para guardar el caracter
+        Aux = Cadena[i];
+        // Necesito cambiar el ultimo caracter para asignarlo al ultimo
+        Cadena[i] = Cadena[Length - i - 1];
+        Cadena[Length - i - 1] = Aux;
+    }
+}
+
+bool CompareStrings(char Cadena1[], char Cadena2[]) {
+    bool Equal = true;
+
+    for (int i = 0; i < strlen(Cadena1); i++) {
+        if (Cadena1[i] != Cadena2[i]) {
+            Equal = false;
+        }
+    }
+
+    return Equal;
+}
+
+int main() {
+
+    printf("Introducir cadena para valorar si es palindromo: \n");
+    fgets(Cadena, 255, stdin);
+    int CadenaLenght = strlen(Cadena);
+    DeleteSpecialCharacters(Cadena);
+
+    int contador = 0;
+
+    for (int i = 0; i < CadenaLenght; i++) {
+        if (i < (CadenaLenght/2) - 1) {
+            FirstWord[i] = Cadena[i];
+        }
+        
+        if (i > (CadenaLenght/2) - 1) {
+            SecondWord[contador] = Cadena[i];
+            contador++;
+        }
+    }
+
+    ReverseWord(SecondWord);
+
+    printf("\n");
+
+    CompareStrings(FirstWord, SecondWord);
+
+    if (CompareStrings) {
+        printf("La palabra es un palindromoo!!!");
+    } else {
+        printf("La palabra no es un palindromo .....");
+    }
+
+    return 0;
+}
---
+++ Exercise_7.cc
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+char Cadena[255] = {'\0'}, SubCadena[255] = {'\0'}, NewCadena[255] = {'\0'};
+
+int strlen(char Cadena[]) {
+    int contador = 0;
+
+    while (Cadena[contador] != '\0') {
+        contador++;
+    }
+
+    return contador;
+}
+
+void DeleteSpecialCharacters(char Cadena[]) {
+
+    Cadena[strlen(Cadena) - 1] = '\0';
+}
+
+void ResetCadena(int Length) {
+    for (int i = 0; i < Length; i++) {
+        SubCadena[i] = '\0'; 
+    }
+}
+
+void InsertCadena(int TimesToInsert, int LengthSubCadena, int IndexCadena) {
+    for (int i = 0; i < TimesToInsert; i++) {
+        for (int j = 0; j < LengthSubCadena; j++) {
+            NewCadena[IndexCadena + j] = SubCadena[j];
+            printf("%c", NewCadena[IndexCadena + j]);
+        }
+
+        IndexCadena += LengthSubCadena - 1;
+    }
+
+}
+
+int main() {
+    printf("Cadena con numeros para multiplicar dicha cadena: ");
+    fgets(Cadena, 255, stdin);
+
+    int number = Cadena[0];
+    int LastCIndex = 0;
+
+    for (int i = 1; i < strlen(Cadena) + 1; i++) {
+
+        if (Cadena[i] < 48 || Cadena[i] > 57 || Cadena[i] != '\0') {
+            SubCadena[i - 1] = Cadena[i];
+        } else {
+            // No entra aqui
+            printf("%d", number);
+            printf("%d", strlen(SubCadena));
+            printf("%d", LastCIndex);
+            InsertCadena(number, strlen(SubCadena), LastCIndex);
+            ResetCadena(strlen(SubCadena));
+            number = Cadena[i];
+            LastCIndex = strlen(NewCadena) - 1;
+        }
+    }
+
+    fputs(NewCadena, stdout);
+}

426ec2c05204dce8b4f42ecf6fed23d896249c53
sanchezcas <sanchezcas@esatvalencia.lan>
2026-01-14 20:20:56 +0100

    add new way to fix it

--- NumeroCadena.cc
+++ NumeroCadena.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 char Cadena[255] = {'\0'}, SubCadena[255] = {'\0'}, NewCadena[255] = {'\0'};
+int subIndex = 1;
 
 int strlen(char Cadena[]) {
     int contador = 0;
@@ -17,49 +18,41 @@ void DeleteSpecialCharacters(char Cadena[]) {
     Cadena[strlen(Cadena) - 1] = '\0';
 }
 
-void ResetCadena(int Length) {
-    for (int i = 0; i < Length; i++) {
-        SubCadena[i] = '\0'; 
-    }
-}
-
-int InsertCadena(int TimesToInsert, int LengthSubCadena, int IndexCadena) {
+int InsertCadena(int TimesToInsert, int SubLength, int IndexCadena) {
+    printf("%d \n", TimesToInsert);
+    printf("%d \n", SubLength);
+    printf("%d \n", IndexCadena);
     for (int i = 0; i < TimesToInsert; i++) {
-        for (int j = 0; j < LengthSubCadena; j++) {
-            NewCadena[IndexCadena + j] = SubCadena[j];
-        }
+        for (int j = 1; j < SubLength + 1; j++) {
 
-        IndexCadena += LengthSubCadena;
+            NewCadena[IndexCadena] = SubCadena[j]; 
+            IndexCadena++;
+        }
     }
 
-    return IndexCadena;
+    // NewCadena[IndexCadena] = SubCadena[IndexSubCadena];
+
+    return strlen(NewCadena);
 }
 
 int main() {
+
     printf("Cadena con numeros para multiplicar dicha cadena: ");
     fgets(Cadena, 255, stdin);
     DeleteSpecialCharacters(Cadena);
 
-    int number = Cadena[0] - 48;
     int LastCIndex = 0;
-    int subIndex = 0;
 
-    for (int i = 1; i < strlen(Cadena); i++) {
+    for (int i = 0; i < strlen(Cadena); i++) {
 
         if (Cadena[i] < 48 || Cadena[i] > 57) {
-            SubCadena[subIndex++] = Cadena[i];
+            SubCadena[subIndex] = Cadena[i];
+            subIndex++;
         } else {
-            // No entra aqui
-            LastCIndex = InsertCadena(number, strlen(SubCadena), LastCIndex);
-            ResetCadena(255);
-            number = Cadena[i] - 48;
+            SubCadena[0] = Cadena[i] - 48;
+            LastCIndex = InsertCadena(SubCadena[0], strlen(SubCadena), LastCIndex);
+            subIndex = 1;
         }
-    }
-
-    if (subIndex > 0) {
-        InsertCadena(number, subIndex, LastCIndex);
-        // ahora sí terminamos la cadena
-        NewCadena[LastCIndex + number * subIndex] = '\0';
     }
 
     fputs(NewCadena, stdout);

199b648c42df7729166b7026bec4ac8b1add9495
sanchezcas <sanchezcas@esatvalencia.lan>
2026-01-20 20:20:32 +0100

    change main code

--- maths/files/TxtToBinario.cc
+++ maths/files/TxtToBinario.cc
@@ -1,34 +1,46 @@
 #include <stdio.h>
 
+struct Info {
+    char Origen[50], Destino[];
+    int KMLongitud;
+};
+
 FILE *f, *distancia;
 int valor;
 char Cadena[255];
 
+int strlen(char Cadena[]) {
+    int contador = 0;
+
+    while (Cadena[contador] != '\0') {
+        contador++;
+    }
+
+    return contador;
+}
+
+void CleanCadena(char Cadena[]) {
+    for (int i = 0; i < strlen(Cadena); i++) {
+        Cadena[i] = '\0';
+    }
+}
+
 int main() {
 
     f = fopen("TablaKM.txt", "r");
-    distancia = fopen("distancies.txt", "w");
+    distancia = fopen("distancies.dat", "wb");
 
     int StartIndex = 0, LastIndex = 0;
+    int contador = 0;
 
     do {
-        char Caracter = fgetc(f);
-        char LastCharacter;
+        // Check de los primeros params, despues vamos a los segundos
 
-        fputc(Caracter, distancia);
-        Cadena[StartIndex] = Caracter;
-        StartIndex++;
+        char Character = fgetc(f);
 
-        if (Caracter == ';' && LastCharacter == ';') {
-            fputc('\n', distancia);
+        if (Character != ';') {
+            Cadena[contador] = Character;
         }
-
-        if (Caracter == ';') {
-            LastCharacter = Caracter;
-        } else {
-            LastCharacter = ' ';
-        }
-
 
     } while (!feof(f));
 

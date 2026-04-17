57713267d4af4f34564139bc0f7201a6f53b34a1
andreuCountry <132867325+andreuCountry@users.noreply.github.com>
2026-04-17 20:15:54 +0200

    collision trial

---
+++ maths/collision.cc
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <esat/math.h>
+#include <esat/draw.h>
+#include <esat/window.h>
+#include <esat/time.h>
+#include <esat/input.h>
+#include <math.h>
+
+
+const int knPoints = 12;
+esat::Vec3 g_circle[knPoints];
+
+void InitCircle() {
+    float angle = 6.28f / (float) knPoints;
+    for (int i = 0; i < knPoints; ++i) {
+        g_circle[i] = {cosf(angle * i), sinf(angle * i), 1.0f};
+    }
+}
+
+void DrawStick() {
+    esat::DrawLine(esat::MousePositionX(), esat::MousePositionY(), esat::MousePositionX() + 100, esat::MousePositionY() + 200);
+}
+
+esat::Mat3 UpdateCircle() {
+    esat::Mat3 m = esat::Mat3Identity();
+    m = esat::Mat3Multiply(esat::Mat3Scale(180.0f, 180.0f), m);
+    m = esat::Mat3Multiply(esat::Mat3Rotate(esat::Time() * 0.001f), m);
+    m = esat::Mat3Multiply(esat::Mat3Translate(200.0f, 300.0f), m);
+
+    return m;
+}
+
+esat::Vec2 FixEq2x2(esat::Vec2 first, esat::Vec2 second) {
+
+    esat::Vec2 result;
+
+    result.x = first.x - first.y;
+    result.y = first.y - first.x;
+}
+
+void DrawCircle(esat::Mat3 m) {
+    esat::DrawSetStrokeColor(0,0,0,255);
+    esat::DrawSetFillColor(255,0,0,255);
+
+    esat::Vec2 tr_circle[knPoints];
+
+    for (int i = 0; i < knPoints; i++) {
+        esat::Vec3 tmp = esat::Mat3TransformVec3(m, g_circle[i]);
+        tr_circle[i] = {tmp.x, tmp.y};
+    }
+    esat::DrawSolidPath(&tr_circle[0].x, knPoints);
+}
+
+int esat::main(int argc, char **argv) {
+
+    esat::WindowInit(800, 600);
+    WindowSetMouseVisibility(true);
+
+    InitCircle();
+
+    while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
+
+    	esat::DrawBegin();
+    	esat::DrawClear(120,120,120);
+
+        esat::Mat3 matriz = UpdateCircle();
+        DrawCircle(matriz);
+        DrawStick();
+        
+    	esat::DrawEnd();
+        esat::WindowFrame();
+        
+    }
+
+    esat::WindowDestroy();
+    
+    return 0;
+}

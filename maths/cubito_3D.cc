efcd73e739462af5e9b6910a117d897f8d5afcaf
andreuCountry <132867325+andreuCountry@users.noreply.github.com>
2026-04-24 23:08:35 +0200

    Update cubito_3D.cc, fixed

--- maths/cubito_3D.cc
+++ maths/cubito_3D.cc
@@ -8,52 +8,85 @@
 
 const int knPoints = 8;
 
+esat::Vec4 g_cube[knPoints];
 
-esat::Vec3 g_cube = {
-
+int edges[12][2] = {
+    {0,1},
+    {1,2},
+    {2,3},
+    {3,0},
+    {4,5},
+    {5,6},
+    {6,7},
+    {7,4},
+    {0,4},
+    {1,5},
+    {2,6},
+    {3,7}
 };
+
+void InitVec() {
+    g_cube[0] = {-1.0f, -1.0f, -1.0f, 1.0f};
+    g_cube[1] = {-1.0f, 1.0f, -1.0f, 1.0f};
+    g_cube[2] = {1.0f, 1.0f, -1.0f, 1.0f};
+    g_cube[3] = {1.0f, -1.0f, -1.0f, 1.0f};
+    g_cube[4] = {-1.0f, -1.0f, 1.0f, 1.0f};
+    g_cube[5] = {-1.0f, 1.0f, 1.0f, 1.0f};
+    g_cube[6] = {1.0f, 1.0f, 1.0f, 1.0f};
+    g_cube[7] = {1.0f, -1.0f, 1.0f, 1.0f};
+}
+
+float angle = 0.0f;
 
 esat::Mat4 MatrizTransform() {
 
-    esat::Vec2 centralPoint = {(float) esat::MousePositionX(), (float) esat::MousePositionY()};
-
     esat::Mat4 m = esat::Mat4Identity();
 
-    
+    angle++;
+
+
+    // rotar al bichito
+
+    m = esat::Mat4Multiply(esat::Mat4RotateX(esat::Time() * 0.001f), m);
+    m = esat::Mat4Multiply(esat::Mat4RotateY(esat::Time() * 0.001f), m);
+    m = esat::Mat4Multiply(esat::Mat4RotateZ(esat::Time() * 0.001f), m);
 
     // Necesitamos transladar por el eje z (T(0, 0, Z))
 
+    m = esat::Mat4Multiply(esat::Mat4Translate(0.0f, 0.0f, 3.0f), m);
 
     // Proyección
 
+    m = esat::Mat4Multiply(esat::Mat4Projection(), m);
 
     // esacalamos con el eje z por 1
 
+    m = esat::Mat4Multiply(esat::Mat4Scale(200.0f, 200.0f, 1.0f), m);
 
     // translacion a efectos por el eje z a 0
 
+    m = esat::Mat4Multiply(esat::Mat4Translate(400.0f, 300.0f, 0.0f), m);
 
     return m;
 }
 
-void InitMatriz() {
-    float angle = 6.28f / (float) knPoints;
-    for (int i = 0; i < knPoints; ++i) {
-        g_cube[i] = {cosf(angle * i), sinf(angle * i), 1.0f};
-    }
-}
-
-void DrawMatriz(esat::Mat3 m) {
+void DrawMatriz(esat::Mat4 m) {
     esat::DrawSetStrokeColor(0,0,0,255);
     esat::DrawSetFillColor(255,0,0,255);
 
-    esat::Vec2 tr_cube[knPoints];
+    esat::Vec3 tr_cube[knPoints];
 
     for (int i = 0; i < knPoints; i++) {
-        esat::Vec3 tmp = esat::Mat3TransformVec3(m, g_cube[i]);
-        tr_cube[i] = {tmp.x, tmp.y};
 
-        esat::DrawLine(&tr_cube[i].x, &tr_cube[i].y, &tr_cube[i+1].x, &tr_cube[i+1].y);
+        // transformacion de vertices en algo temporal
+        esat::Vec4 tmp = esat::Mat4TransformVec4(m, g_cube[i]);
+
+        tr_cube[i].x = tmp.x / tmp.w;
+        tr_cube[i].y = tmp.y / tmp.w;
+    }
+
+    for (int i = 0; i < 12; i++) {
+        esat::DrawLine(tr_cube[edges[i][0]].x, tr_cube[edges[i][0]].y,tr_cube[edges[i][1]].x, tr_cube[edges[i][1]].y);
     }
 }
 
@@ -62,13 +95,14 @@ int esat::main(int argc, char **argv) {
     esat::WindowInit(800, 600);
     WindowSetMouseVisibility(true);
     
+    InitVec();
 
     while(esat::WindowIsOpened() && !esat::IsSpecialKeyDown(esat::kSpecialKey_Escape)) {
 
     	esat::DrawBegin();
     	esat::DrawClear(120,120,120);
 
-        esat::Mat3 matriz = UpdateCircle();
+        esat::Mat4 matriz = MatrizTransform();
         DrawMatriz(matriz);
         
     	esat::DrawEnd();
